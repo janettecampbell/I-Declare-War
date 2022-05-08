63a3717734dc057744bffbcd0f87d12db48d100c
Janette Campbell <campbjp@hotmail.com>
2022-05-08 17:33:32 -0400

    added back of playing card and renamed grid spaces

--- js/script.js
+++ js/script.js
@@ -51,15 +51,18 @@ console.log(deck);
 // Decide to split deck or give each player a full deck and remove randomly pulled card from both decks
 let playerHand;
 let computerHand;
+let currentCard;
 
 const dealHand = () => {
   playerHand = deck.slice(0, 26);
   computerHand = deck.slice(26);
+  currentCard = 0;
 };
 dealHand();
 console.log(playerHand);
 console.log(computerHand);
-// select "top" card to play each round
+// select "top" cards to play each round
+
 // highest card wins
 // put cards back in winners deck in random position
 // in event of a tie add 4 cards from each player continue until there is not a tie
